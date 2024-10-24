7488de7a2f0bc159ef3a2beb93b81d80ffe69a93
danielwerg <35052399+danielwerg@users.noreply.github.com>
2024-10-24 13:52:35 +0300

    fix: handle empty fields

--- main.go
+++ main.go
@@ -69,10 +69,18 @@ func getSongInfo(player string) (string, error) {
 		return "Unexpected output format.", nil
 	}
 
+	emptyField := "—" // em dash
+
 	// Truncate the title, artist, and album to the specified max length
 	title := truncateText(strings.TrimSpace(info[0]), maxTitleLength)
 	artist := truncateText(strings.TrimSpace(info[1]), maxArtistLength)
+	if artist == "" {
+		artist = emptyField
+	}
 	album := truncateText(strings.TrimSpace(info[2]), maxAlbumLength)
+	if album == "" {
+		album = emptyField
+	}
 	status := strings.TrimSpace(info[3])
 
 	// Get song length
